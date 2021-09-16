9c06c33716ab36d9643d1a3df1813fe93c7a4651
rlahfld54 <rlahfld54@naver.com>
2021-09-16 13:42:20 +0900

    수정

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    //gnb open
+    $(".about-sidiz").click(function () {
+        $("#about-sidiz").toggle();
+    });
+
+    $(".products").click(function () {
+        $("#products").toggle();
+    });
+
+    $(".support").click(function () {
+        $("#support").toggle();
+    });
+});
