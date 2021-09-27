1e884f34bda632912664dc960df2eec332ad65da
rlahfld54 <rlahfld54@naver.com>
2021-09-27 18:33:24 +0900

    수정 중

--- js/script.js
+++ js/script.js
@@ -1,14 +1,45 @@
 $(document).ready(function () {
-    //gnb open
-    $(".about-sidiz").click(function () {
-        $("#about-sidiz").toggle();
+    //안내창
+    let modal = $('.modal');
+    let modal_box = $('.modal-box');
+    let modal_close = $('.madal-close');
+
+    modal.click(function () {
+        $(this).fadeOut();
     });
 
-    $(".products").click(function () {
-        $("#products").toggle();
-    });
 
-    $(".support").click(function () {
-        $("#support").toggle();
-    });
+  $(".about-sidiz").click(function () {
+    $("#about-sidiz").toggle();
+    $("#products").hide();
+    $("#support").hide();
+  });
+
+  $(".products").click(function () {
+    $("#about-sidiz").hide();
+    $("#products").toggle();
+    $("#support").hide();
+  });
+
+  $(".support").click(function () {
+    $("#about-sidiz").hide();
+    $("#products").hide();
+    $("#support").toggle();
+  });
+
+  $(".about-sidiz").mouseenter(function () {
+    $("#about-sidiz").toggle();
+    $("#products").hide();
+    $("#support").hide();
+  });
+  $(".products").mouseenter(function () {
+    $("#about-sidiz").hide();
+    $("#products").toggle();
+    $("#support").hide();
+  });
+  $(".support").mouseenter(function () {
+    $("#about-sidiz").hide();
+    $("#products").hide();
+    $("#support").toggle();
+  });
 });
